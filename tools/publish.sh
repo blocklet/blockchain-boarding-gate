2c79e7030c9cfd3a9f022e94a8ba13673c0d21dd
linchen1987 <linchen.1987@foxmail.com>
2020-11-02 21:22:05 +0800

    fix ci

--- tools/publish.sh
+++ tools/publish.sh
@@ -1,14 +1,17 @@
+set -e
+
 VERSION=$(cat version | awk '{$1=$1;print}')
 echo "publish version ${VERSION}"
 
 git config --local user.name "wangshijun"
 git config --local user.email "wangshijun2010@gmail.com"
 
+
+echo "publishing to npm..."
 make release
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
 sudo npm install -g @abtnode/cli
-
-echo "publishing wallet demo blocklet..."
+echo "SKIP_PREFLIGHT_CHECK=true" > ./.env
 npm run build
 npm publish
 
