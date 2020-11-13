1fc4170fdc40bfe81d51da287a208f8c0ba62cef
wangshijun <wangshijun2010@gmail.com>
2020-11-13 21:02:27 +0800

    chore: migrate to blocklet.yml (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,10 @@
+## 1.1.1 (November 12, 2020)
+
+
+
+## 1.1.0 (November 11, 2020)
+
+- chore: update publish script
+- chore: migrate to blocklet.yml
+
 ## 1.0.22 (October 02, 2020)
-
-
-
--- tools/publish.sh
+++ tools/publish.sh
@@ -6,7 +6,6 @@ echo "publish version ${VERSION}"
 git config --local user.name "wangshijun"
 git config --local user.email "wangshijun2010@gmail.com"
 
-
 echo "publishing to npm..."
 make release
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
