3a91c183ff9491009f679e560c03e2db05429b0e
polunzh <polunzh@gmail.com>
2021-01-14 10:23:18 +0800

    feat: support publish to blocklet registry

--- tools/publish.sh
+++ tools/publish.sh
@@ -3,9 +3,6 @@ set -e
 VERSION=$(cat version | awk '{$1=$1;print}')
 echo "publish version ${VERSION}"
 
-git config --local user.name "wangshijun"
-git config --local user.email "wangshijun2010@gmail.com"
-
 echo "publishing to npm..."
 make release
 npm config set '//registry.npmjs.org/:_authToken' "${NPM_TOKEN}"
@@ -13,6 +10,10 @@ sudo npm install -g @abtnode/cli
 echo "SKIP_PREFLIGHT_CHECK=true" > ./.env
 npm run bundle
 npm publish .blocklet/bundle
+
+echo "publishing to blocklet registry"
+blocklet config registry ${BLOCKLET_REGISTRY}
+blocklet publish --developer-sk ${ABTNODE_DEV_SK}
 
 # deploy to remote ABT Node
 set +e
@@ -39,6 +40,4 @@ if [ "${AWS_NODE_ENDPOINT}" != "" ]; then
   fi
 fi
 
-# trigger ArcBlock/blocklets repo release
-echo "trigger ArcBlock/blocklets repo release"
-.makefiles/trigger_registry_build.sh
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${NAME} v${VERSION} was successfully published\"}" ${SLACK_WEBHOOK}
